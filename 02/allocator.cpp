41aa55d70daec3c433777dcc621953bf5ad7f564
Nikolay <xsile2009@gmail.com>
2019-10-20 15:47:21 +0300

    HW2: LinearAllocator

---
+++ 02/allocator.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+class LinearAllocator {
+private:
+    size_t memory_left;
+    size_t offset;
+    char *head_block_ptr;
+
+public:
+    LinearAllocator(size_t maxSize);
+    char *alloc(size_t size);
+    void reset();
+    ~LinearAllocator();
+};
+
+LinearAllocator::LinearAllocator(size_t maxSize) {
+    memory_left = maxSize;
+    offset = 0;
+    head_block_ptr = (char *) malloc(maxSize);
+}
+
+char *LinearAllocator::alloc(size_t size) {
+    if (size > memory_left) {
+        return nullptr;
+    }
+    memory_left -= size;
+    offset += size;
+    return head_block_ptr + offset - size;
+}
+
+void LinearAllocator::reset() {
+    memory_left += offset;
+    offset = 0;
+}
+
+// unnecessary hear bud good practice to free memory for each malloc
+LinearAllocator::~LinearAllocator() {
+    free(head_block_ptr);
+}
---
+++ 02/test.cpp
@@ -0,0 +1,33 @@
+#include "allocator.h"
+#include <cassert>
+
+int main() {
+    LinearAllocator allocator{sizeof(int) * 100};
+
+    int *arr1 = (int *) allocator.alloc(sizeof(int) * 5);
+    assert(arr1 != nullptr);
+    int *arr2 = (int *) allocator.alloc(sizeof(int) * 50);
+    assert(arr2 != nullptr);
+    assert(arr2 == arr1 + 5);
+    int *arr3 = (int *) allocator.alloc(sizeof(int) * 45);
+    assert(arr3 != nullptr);
+    assert(arr3 == arr2 + 50);
+    int *arr4 = (int *) allocator.alloc(sizeof(int) * 1);
+    assert(arr4 == nullptr);
+    allocator.reset();
+    int *arr5 = (int *) allocator.alloc(sizeof(int) * 10);
+    assert(arr5 != nullptr);
+    int *arr6 = (int *) allocator.alloc(sizeof(int) * 90);
+    assert(arr6 == arr5 + 10);
+    allocator.reset();
+    arr5[0] = 1;
+    arr5[9] = 1;
+    arr5[10] = 10;
+    assert(arr5[10] == 10);
+    arr6[0] = 11;
+    assert(arr5[10] == 11);
+    assert(arr1 == arr5);
+
+    std::cout << "Tests passed!" << std::endl;
+    return EXIT_SUCCESS;
+}
